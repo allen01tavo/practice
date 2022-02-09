7359a7f0856d14fd674fb447aa6f383ba34aeefb
Gus Maturana <gumaturana@gmail.com>
2022-02-08 21:21:59 -0500

    Initial Commit

---
+++ Program_8_1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Program_8_1
+//
+//  Created by Gus Maturana on 2/8/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
